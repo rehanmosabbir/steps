d8ba8090d86f0592807c65d4df9eb26e548f15ef
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-07 11:22:14 +0600

    handle next and previous button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,14 @@ const messages = [
 ];
 
 export default function App() {
-  let step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +26,16 @@ export default function App() {
         Step {step} : {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
